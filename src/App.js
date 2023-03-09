0f1462499925c51da80f10281739f79bc709a4e7
Ionut Catalin Tatuta <catalin.tatuta@softbinator.com>
2023-03-09 19:28:06 +0200

    Add logic for first react class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ClassComponent from './ClassComponent';
 
 function App() {
+  const [num, setNum] = useState(10)
+
+  const [list, setList] = useState([10])
+
+  const buttonHandler = () => {
+      setNum(num+1);
+      console.log(list);
+  }
+
+  const buttonHandler2 = () => {
+      setList([...list, num]);
+      console.log(list);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{border: '10px solid red', color: 'red'}}>
+      <h1>App component</h1>
+      <p>Number from useState: {list.length}</p>
+      <p>List from useState: {list}</p>
+      <button onClick={buttonHandler} >Increase number</button>
+      <button onClick={buttonHandler2} >Add it to the list</button>
+      <ClassComponent numberProp={num} aStringProp="a string value">
+        <span><em>This value is from the 'children' prop</em></span>
+      </ClassComponent>
+      
     </div>
   );
 }
---
+++ src/ClassComponent.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+class ClassComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            salutation: 'hello',
+            time: Date.now(),
+            countedNumber: props.numberProp,
+        }
+    }
+
+    componentDidMount() {
+        alert('Initial number from state:' + this.state.countedNumber);
+        this.setState({countedNumber: 1000});
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevProps.numberProp !== this.props.numberProp) {
+            this.setState({salutation: 'Hey there, The number prop changed to: ' + this.props.numberProp});
+        }
+    }
+
+    doSomething = () => {
+        this.setState({
+            time: Date.now()
+        })
+    }
+
+    render() {
+        const { salutation, countedNumber, time } = this.state;
+        
+        const { aStringProp, numberProp, children } = this.props;
+    
+        return (
+            <div style={{border: '5px solid blue', color: 'blue'}}>
+                <h2>Class Component</h2>
+                <h3>State</h3>
+                <p>
+                    {salutation}
+                </p>
+                <p>
+                    This is the number from state: {countedNumber}
+                </p>
+                <p>
+                    <button onClick={this.doSomething}>update time</button>
+                    <span> unix time: {time}</span>
+                </p>
+                <br/>
+                <br/>
+                <h3>Props</h3>
+                <p>
+                    {aStringProp}
+                </p>
+                <p>
+                    {numberProp}
+                </p>
+                <p>
+                    {children}
+                </p>
+                
+            </div>
+        )
+    }
+}
+
+export default ClassComponent
