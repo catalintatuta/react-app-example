b307899d8b577033428d2340cc26aad1472ac5ce
Ionut Catalin Tatuta <catalin.tatuta@softbinator.com>
2023-03-20 14:49:51 +0200

    Move Functional component logic from App

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import { useState } from 'react';
 import './App.css';
-import ClassComponent from './ClassComponent';
+import FunctionComponent from "./FunctionComponent";
 
 function App() {
-  const [num, setNum] = useState(10)
-
-  const [list, setList] = useState([10])
-
-  const buttonHandler = () => {
-      setNum(num+1);
-  }
-
-  const buttonHandler2 = () => {
-      setList([...list, num]);
-  }
-
   return (
-    <div className="App" style={{border: '10px solid red', color: 'red'}}>
-      <h1>App component</h1>
-      <p>Number from useState: {num}</p>
-      <p>List from useState: {list}</p>
-      <button onClick={buttonHandler} >Increase number</button>
-      <button onClick={buttonHandler2} >Add it to the list</button>
-      <ClassComponent numberProp={num} aStringProp="a string value">
-        <span><em>This value is from the 'children' prop</em></span>
-      </ClassComponent>
-      
+    <div className="App">
+      <FunctionComponent />
     </div>
   );
 }
---
+++ src/FunctionComponent.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import ClassComponent from "./ClassComponent";
+
+const FunctionComponent = () => {
+  const [num, setNum] = useState(10)
+
+  const [list, setList] = useState([10])
+
+  const buttonHandler = () => {
+    setNum(num+1);
+  }
+
+  const buttonHandler2 = () => {
+    setList([...list, num]);
+  }
+
+  return (
+    <div className="App" style={{border: '10px solid red', color: 'red'}}>
+      <h1>Function component</h1>
+      <p>Number from useState: {num}</p>
+      <p>List from useState: {list}</p>
+      <button onClick={buttonHandler} >Increase number</button>
+      <button onClick={buttonHandler2} >Add it to the list</button>
+      <ClassComponent numberProp={num} aStringProp="a string value">
+        <span><em>This value is from the 'children' prop</em></span>
+      </ClassComponent>
+    </div>
+  );
+};
+
+export default FunctionComponent;
